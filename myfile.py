7befb53834a6d57aaaa2b231b01747d7a9450c0a
Ryder-oss <24ryanr@mountainview.school.nz>
2026-02-17 13:21:11 +1300

    Create myfile.py

---
+++ myfile.py
@@ -0,0 +1,15 @@
+sword_damage = 10
+start_health = 100
+end_health = start_health - sword_damage
+
+print(f"Sam's health is: {start_health}")
+print(f"Sam takes {sword_damage} damage...")
+print(f"Sam's health is: {end_health}")
+
+apple_edible= 10
+start_hunger= 100
+end_hunger = start_hunger - apple_edible
+
+print(function"Sam's hunger is: {start_hunger}")
+print(function"Sam takes {apple_edible} edible...")
+print(function"sam's hunger is: {end_hunger}")
