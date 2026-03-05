acec2765096e2824f4bbef7022d57fb329c4a069
Ryder-oss <24ryanr@mountainview.school.nz>
2026-03-06 10:39:34 +1300

    Update -- SQLite.sql

--- -- SQLite.sql
+++ -- SQLite.sql
@@ -6,11 +6,11 @@
 --1
 
 
-Select * FROM student WHERE first_name = 'Gene';
+Select * FROM Student WHERE first_name = 'Gene';
 
-Select * FROM student ORDER BY date_of_birth;
+Select * FROM Student ORDER BY date_of_birth;
 
-SELECT * FROM student WHERE last_name LIKE '%Y';
+SELECT * FROM Student WHERE last_name LIKE '%Y';
 
 SELECT * FROM Class WHERE NOT room_id = '44';
 
@@ -20,6 +20,10 @@ Select * FROM Class WHERE room_id > '43';
 
 SELECT teacher_name, room_id FROM Class
     INNER
-     JOIN Teacher ON class.teacher_id = Teacher.teacher_id
+     JOIN Teacher ON Class.teacher_id = Teacher.teacher_id
 WHERE room_id = 43 or room_id = 44;
 
+SELECT student_id, class_id FROM StudentClass
+    INNER
+     JOIN Student ON StudentClass.student_id = Student.student_id
+WHERE class_id = 44;
